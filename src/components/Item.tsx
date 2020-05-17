0ce92231bdc32a2063fcf00f16ca28f0f283ad88
ozturkcangokkaya <ozturkcangokkaya@gmail.com>
2020-05-18 00:54:04 +0300

    fix indentation

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -11,124 +11,124 @@ export interface ItemProps {
 }
 
 const Container = styled.div`
-	padding: 10px;
-	border: 1px solid #f0f0f0;
-	border-radius: 10px;
-	display: flex;
-	flex-direction: column;
-	cursor: pointer;
+  padding: 10px;
+  border: 1px solid #f0f0f0;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
 
-	&:hover {
-		border-color: aqua;
-	}
+  &:hover {
+    border-color: aqua;
+  }
 `;
 
 const ImageContainer = styled.div`
-	width: 100%;
-	padding-bottom: 100%;
-	position: relative;
+  width: 100%;
+  padding-bottom: 100%;
+  position: relative;
 `;
 
 const Image = styled.img`
-	width: 100%;
-	height: 100%;
-	object-fit: contain;
-	position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  position: absolute;
 `;
 
 const Name = styled.div`
-	font-size: 14px;
-	margin-top: 20px;
-	text-align: center;
+  font-size: 14px;
+  margin-top: 20px;
+  text-align: center;
 `;
 
 const Details = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const DetailsImage = styled.img`
-	width: 300px;
-	height: 300px;
-	margin-right: 20px;
-	object-fit: contain;
+  width: 300px;
+  height: 300px;
+  margin-right: 20px;
+  object-fit: contain;
 `;
 
 const StatusMsg = styled.div`
-	font-size: 18px;
-	text-align: center;
+  font-size: 18px;
+  text-align: center;
 `;
 
 const DetailsTitle = styled.h3`
-	margin-bottom: 10px;
+  margin-bottom: 10px;
 `;
 
 const GET_POKEMON_DETAILS = gql`
-	query PokemonDetails($id: String!){
-		pokemon(id: $id){
-			maxHP
-			maxCP
-			resistant
-		}
-	}
+  query PokemonDetails($id: String!){
+    pokemon(id: $id){
+      maxHP
+      maxCP
+      resistant
+    }
+  }
 `
 
 const Item: FC<ItemProps> = props => {
-	const {id, image, name} = props;
-	const [modalState, setModalState] = useState(false);
+  const {id, image, name} = props;
+  const [modalState, setModalState] = useState(false);
   const [getPokemonDetails, { loading, error, data }] = useLazyQuery(GET_POKEMON_DETAILS);
-	
-	const handleClick = (id: string) => {
-		getPokemonDetails({ variables: { id: id } })
-		setModalState(true);
-	}
+  
+  const handleClick = (id: string) => {
+    getPokemonDetails({ variables: { id: id } })
+    setModalState(true);
+  }
 
   return (
-		<>
-			<Container key={id} onClick={() => handleClick(id)}>
-				<ImageContainer>
-					<Image src={image} alt={name} />
-				</ImageContainer>
-				<Name>{name}</Name>
-			</Container>
+    <>
+      <Container key={id} onClick={() => handleClick(id)}>
+        <ImageContainer>
+          <Image src={image} alt={name} />
+        </ImageContainer>
+        <Name>{name}</Name>
+      </Container>
 
-			<Modal isOpen={modalState} onClose={() => setModalState(false)}>
-				{loading && <StatusMsg>Loading details of <b>{name}...</b></StatusMsg>}
-				{error && <StatusMsg>Error! ${error.message}</StatusMsg>}
+      <Modal isOpen={modalState} onClose={() => setModalState(false)}>
+        {loading && <StatusMsg>Loading details of <b>{name}...</b></StatusMsg>}
+        {error && <StatusMsg>Error! ${error.message}</StatusMsg>}
 
-				{data?.pokemon && (
-					<Details>
-						<DetailsImage src={image} alt={name} />
-						<div>
-							<DetailsTitle>{name}</DetailsTitle>
-							<div>
-								<span role="img" aria-label="Maximum Health">
-									❤️
-								</span>
-								MaxHP: {data.pokemon.maxHP}
-							</div>
-							<div>
-								<span role="img" aria-label="Combat Power">
-									⚔️ 
-								</span>
-								MaxCP: {data.pokemon.maxCP}
-							</div>
+        {data?.pokemon && (
+          <Details>
+            <DetailsImage src={image} alt={name} />
+            <div>
+              <DetailsTitle>{name}</DetailsTitle>
+              <div>
+                <span role="img" aria-label="Maximum Health">
+                  ❤️
+                </span>
+                MaxHP: {data.pokemon.maxHP}
+              </div>
+              <div>
+                <span role="img" aria-label="Combat Power">
+                  ⚔️ 
+                </span>
+                MaxCP: {data.pokemon.maxCP}
+              </div>
 
-							{data.pokemon.resistant.length && (
-								data.pokemon.resistant.map((item:string) => (
-									<div key={item}>
-										<span role="img" aria-label="Checkmark">
-											✅
-										</span>
-										{item} Resistance
-									</div>
-								))
-							)}
-						</div>
-					</Details>
-				)}
-			</Modal>
-		</>
+              {data.pokemon.resistant.length && (
+                data.pokemon.resistant.map((item:string) => (
+                  <div key={item}>
+                    <span role="img" aria-label="Checkmark">
+                      ✅
+                    </span>
+                    {item} Resistance
+                  </div>
+                ))
+              )}
+            </div>
+          </Details>
+        )}
+      </Modal>
+    </>
   )
 }
 
--- src/components/list.tsx
+++ src/components/list.tsx
@@ -3,31 +3,31 @@ import styled from 'styled-components';
 import Item, { ItemProps } from './Item';
 
 interface ListProps {
-	items: ItemProps[];
+  items: ItemProps[];
 }
 
 const Wrapper = styled.div`
-	width: 100%;
-	display: grid;
-	grid-template-columns: repeat(12, 1fr);
-	grid-gap: 20px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-gap: 20px;
 `;
 
 const List: FC<ListProps> = (props) => {
-	const { items } = props;
+  const { items } = props;
 
-	return (
-		<Wrapper>
-			{items.map((pokemon: ItemProps) => (
-				<Item
-					key={pokemon.id}
-					id={pokemon.id}
-					name={pokemon.name}
-					image={pokemon.image}
-				/>
-			))}
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      {items.map((pokemon: ItemProps) => (
+        <Item
+          key={pokemon.id}
+          id={pokemon.id}
+          name={pokemon.name}
+          image={pokemon.image}
+        />
+      ))}
+    </Wrapper>
+  );
 };
 
 export default List;
